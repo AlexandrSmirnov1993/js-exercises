3caf416bee27338fff6b03019775542070f20130
Alexandr Smirnov <bearmd93@gmail.com>
2024-04-11 19:30:39 +0300

    is happy ticket

---
+++ isHappyTicket.js
@@ -0,0 +1,27 @@
+// isHappyTicket('385916'); // true
+// isHappyTicket('231002'); // false
+// isHappyTicket('1222');   // false
+// isHappyTicket('054702'); // true
+// isHappyTicket('00');     // true
+
+function isHappyTicket(num) {
+  let digits = num.split("");
+  let firstHalf = 0;
+  let secondHalf = 0;
+
+  for (let i = 0; i < digits.length / 2; i++) {
+    firstHalf += parseInt(digits[i]);
+  }
+
+  for (let i = digits.length - 1; i >= digits.length / 2; i--) {
+    secondHalf += parseInt(digits[i]);
+  }
+
+  return firstHalf === secondHalf;
+}
+
+console.log(isHappyTicket("385916")); // true
+console.log(isHappyTicket("231002")); // false
+console.log(isHappyTicket("1222")); // false
+console.log(isHappyTicket("054702")); // true
+console.log(isHappyTicket("00")); // true
