17ed3cea5203a9d89deb8fa97aed1ebd597fae0a
Alexandr Smirnov <bearmd93@gmail.com>
2024-04-16 21:19:59 +0300

    up

--- invertCase.js
+++ invertCase.js
@@ -3,3 +3,29 @@
 // invertCase('Hello, World!'); // hELLO, wORLD!
 // invertCase('I loVe JS');     // i LOvE js
 
+// 1-st variant
+// function invertCase(str) {
+//   let result = "";
+//   for (let i = 0; i < str.length; i++) {
+//     let letter = str[i];
+//     letter === letter.toLowerCase()
+//       ? (result += letter.toUpperCase())
+//       : (result += letter.toLowerCase());
+//   }
+//   return result;
+// }
+
+// 2-nd variant
+function invertCase(str) {
+  return str
+    .split("")
+    .map((letter) =>
+      letter === letter.toLowerCase()
+        ? letter.toUpperCase()
+        : letter.toLowerCase()
+    )
+    .join("");
+}
+
+console.log(invertCase("Hello, World!"));
+console.log(invertCase("I loVe JS"));
