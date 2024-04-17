c7d3be13c6d38c3d8c2583ea54d4e6c8ab9c505a
Alexandr Smirnov <bearmd93@gmail.com>
2024-04-17 22:01:21 +0300

    up

---
+++ isHappyNumber.js
@@ -0,0 +1,15 @@
+// Let's call happy numbers those that, as a result of a series of transformations of the form 'sum of the squares of digits', will turn into one. For example, for the number 7, the transformation chain will look like this:
+
+//Examples
+
+// 7   => 7^2 = 49,
+// 49  => 4^2 + 9^2 = 16 + 81 = 97,
+// 97  => 9^2 + 7^2 = 81 + 49 = 130,
+// 130 => 1^2 + 3^2 + 0^2 = 10,
+// 10  => 1^2 + 0^2 = 1.
+
+// Conclusion: number 7 - is happy.
+
+function isHappyNumber(num) {}
+
+console.log(isHappyNumber(7));
