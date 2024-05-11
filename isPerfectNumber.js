7c8000e19a7d247cb59329498c110cda71397d38
Alexandr Smirnov <bearmd93@gmail.com>
2024-05-11 19:37:37 +0300

    Up

---
+++ isPerfectNumber.js
@@ -0,0 +1,9 @@
+// In number theory, a perfect number is a positive integer that is equal to the sum of its positive proper divisors, that is, divisors excluding the number itself.
+
+//Examples
+
+// For instance, 6 has proper divisors 1, 2 and 3, and 1 + 2 + 3 = 6, so 6 is a perfect number. The next perfect number is 28, since 1 + 2 + 4 + 7 + 14 = 28.
+
+function isPerfectNumber(num) {}
+
+console.log(isPerfectNumber(28));
