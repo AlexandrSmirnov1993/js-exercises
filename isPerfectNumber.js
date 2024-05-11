891f8aaa191aaea48a35c4bcfb7b66b1ddbd8ba3
Alexandr Smirnov <bearmd93@gmail.com>
2024-05-11 19:51:52 +0300

    Up

--- isPerfectNumber.js
+++ isPerfectNumber.js
@@ -4,6 +4,19 @@
 
 // For instance, 6 has proper divisors 1, 2 and 3, and 1 + 2 + 3 = 6, so 6 is a perfect number. The next perfect number is 28, since 1 + 2 + 4 + 7 + 14 = 28.
 
-function isPerfectNumber(num) {}
+function isPerfectNumber(num) {
+  let divisibleNum = 0;
+  let sum = 0;
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0) {
+      divisibleNum = i;
+      sum += divisibleNum;
+    }
+  }
+  return sum === num ? true : false;
+}
 
+console.log(isPerfectNumber(6));
+console.log(isPerfectNumber(7));
 console.log(isPerfectNumber(28));
+console.log(isPerfectNumber(29));
