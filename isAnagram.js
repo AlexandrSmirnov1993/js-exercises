57fd48586978b4b5cf3f159b0daa1cc0c500d60e
Alexandr Smirnov <bearmd93@gmail.com>
2024-05-28 22:39:17 +0300

    inProcess

---
+++ isAnagram.js
@@ -0,0 +1,11 @@
+// An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.  An anagram does not have to have the same meaning as the original word or phrase.
+
+//Examples
+// cat - act
+//listen - silent
+//triangle - integral
+//Statue of Liberty - Built to stay free
+
+function isAnagram(str1, str2) {}
+
+console.log(isAnagram("listen", "silent"));
